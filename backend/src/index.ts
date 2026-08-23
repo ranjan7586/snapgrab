2b1fffd97ccbab09ffc912d293e68a5b257c0434
Ranjan Jana <111300453+ranjan7586@users.noreply.github.com>
2026-08-23 22:49:40 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ const PORT = Number(process.env.PORT || 8080);
 const allowedOrigins = (process.env.FRONTEND_URL || "http://localhost:3000")
   .split(",")
   .map((s) => s.trim());
-
+console.log(allowedOrigins)
 app.use(helmet());
 app.use(morgan("tiny"));
 app.use(express.json({ limit: "100kb" }));
